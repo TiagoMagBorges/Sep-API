24eab64887d3f6588853bdd6985cd00bc89bd3b8
tiagomagborges <tiagomagborges@gmail.com>
2026-03-06 18:39:24 -0300

    first commit

---
+++ src/main/java/com/necklogic/sepapi/SepApiApplication.java
@@ -0,0 +1,13 @@
+package com.necklogic.sepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/necklogic/sepapi/SepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.necklogic.sepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
